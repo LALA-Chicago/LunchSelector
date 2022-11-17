2f424beb0c8f2761b248caaaa09dc907f73042f9
Lizandro <businesslizandro@gmail.com>
2022-11-16 22:37:27 -0600

    added a new menu and items folder, added the auth file and some new files

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
 import React from 'react'
-// import { BrowserRouter, Routes, Route, link} from 'react-router-dom'
+import { HashRouter, Routes, Route, link} from 'react-router-dom'
 // import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 // import { setContext } from '@apollo/client/link/context'
-import Login from './components/Login'
 import Main from './components/Main'
-import NavBar from './components/NavBar'
-import SavedFavs from './components/SavedFavs'
-import logo from './logo.svg';
+import NavBar from './components/menu/NavBar'
+import Section from './components/items/Section'
 import './App.css';
 
 function App() {
   return (
+    //the navbar and section on lines 13 and 14 should be removed
+    //truing to get them on main to only have main here but i cant get it yet
+
+  <HashRouter>
+    <NavBar />
+    <Section />
     <div className="App">
-      <header className="App-header">
-      <Main />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Header /> */}
     </div>
+    <Routes>
+      {/* <Main /> */}
+      <Route path='' element={<Main />} />
+      {/* <Route path='about' element={<About />} />
+      <Route path='portfolio' element={<Projects />} /> */}
+    </Routes>
+  </HashRouter>
   );
 }
 
--- src/components/menu/NavBar.js
+++ src/components/menu/NavBar.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-import SignUpForm from './SignupForm';
-import LoginForm from './LoginForm';
-
-
+import SignUpForm from '../SignUp';
+import LoginForm from '../Login';
+import Auth from '../../utils/auth';
 
   const NavBar = () => {
     // set modal display state
