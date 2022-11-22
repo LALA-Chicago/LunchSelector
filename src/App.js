2c684279236b57601a0177a8193a2d182f615ccc
Angel Escobar <escobar@Angels-MacBook-Pro-162.local>
2022-11-21 21:29:15 -0600

    package built

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import { HashRouter} from 'react-router-dom'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
 import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context'
 import Main from './components/Main'
 import NavBar from './components/NavBar'
 import Form from './components/Form'
-import Nav from './components/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-// const auth = require("./middleware/auth");
 
-// const httpLink = createHttpLink({
-//   uri: '/graphql',
-// });
 
 const url = process.env.NODE_ENV === 'development'
     ? '/graphql' : 'https://boiling-badlands-98104.herokuapp.com/';
@@ -44,8 +37,7 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    //the navbar and section on lines 13 and 14 should be removed
-    //truing to get them on main to only have main here but i cant get it yet
+    
   <ApolloProvider client={client}>
   <HashRouter>
    
@@ -62,17 +54,6 @@ function App() {
 
       <br></br>
 
-          {/* <nav>
-            <Link to='login'>Login</Link>
-            <Link to='signup'>Sign Up</Link>
-          </nav>
-          <Routes>
-            <Route path='login' element={<Login />} />
-            <Route path='signup' element={<SignUp />} />
-          </Routes> */}
-        
-
-      
     </div>
     
   </HashRouter>
